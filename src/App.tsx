0424e8dfbbc4c9928f003697335119b85601a6bf
ViktorVan22 <zcg5368@163.com>
2022-06-23 22:28:16 +0800

    optimized routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "views/Home";
 import { Money } from "views/Money";
 import { Statistics } from "views/Statistics";
 import { Labels } from "views/Labels";
 import { NewTag } from "views/NewTag";
+import { NoMatch } from "views/NoMatch";
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="/home" element={<Home />} />
           <Route path="/money" element={<Money />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/labels" element={<Labels />} />
           <Route path="/newTag" element={<NewTag />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </HashRouter>
     </div>
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const NoMatch = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/home");
+    }, 3000);
+  }, []);
+  return <div>Oops..! 来到了陌生的地方，即将跳转回首页...</div>;
+};
+
+export { NoMatch };
