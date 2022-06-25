e6ce3b5a16e9a5f27c70e211457aa7e6c6deaf89
ViktorVan22 <zcg5368@163.com>
2022-06-25 11:19:50 +0800

    添加newTag CSS样式

--- src/views/NewTag.tsx
+++ src/views/NewTag.tsx
@@ -1,7 +1,130 @@
+import { Icon } from "components/Icon";
 import "styles/views/newTag.scss";
 
 const NewTag = () => {
-  return <div>NewTag.tsx</div>;
+  return (
+    <div className="newTag-layout">
+      <header className="newTagHeader">
+        <Icon name="cancel" />
+        <span>新增自定义分类</span>
+        <Icon name="check" />
+      </header>
+      <main className="newTagMain">
+        <div className="option">
+          <span>支出</span>
+          <span>收入</span>
+        </div>
+        <div className="editTag">
+          <div className="chosenIcon">选择的图标</div>
+          <input type="text" placeholder="输入标签名称" />
+        </div>
+        <ul className="availableIcons">
+          <li>
+            <Icon name="dining" />
+          </li>
+          <li>
+            <Icon name="snack" />
+          </li>
+          <li>
+            <Icon name="motel" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+          <li>
+            <Icon name="shopping" />
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 };
 
 export { NewTag };
